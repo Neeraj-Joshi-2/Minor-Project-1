48a2326c30ee8b1eea040ba0ff865c67bd0d433a
Neeraj Joshi <79934749+Neeraj-Joshi-2@users.noreply.github.com>
2021-11-02 11:46:42 +0530

    with Neural Netwroks

--- src/studentclassificationsystem/Classify.java
+++ src/studentclassificationsystem/Classify.java
@@ -8,17 +8,18 @@ public class Classify {
     static int label[][] = new int[1100][3] ;
     static int trainData[][] = new int[800][4], testData[][] = new int[250][4] ;
     static int trainLabel[][] = new int[800][3], testLabel[][] = new int[250][3] ;
-    static int len ,n_x=4,n_y=3,n_h=800 , iterations = 10;
+    static int len ,n_x=4,n_y=3,n_h=800 , iterations = 1000;
     static double w1[][] = new double[n_h][n_x] ; 
     static double b1[][] = new double[n_h][1] ;
     static double w2[][] = new double[n_y][n_h] ; 
     static double b2[][] = new double[n_y][1] ;
-    static double learningRate = 0.02 ;
+    static double learningRate = 0.002 ;
     static double z1[][] = new double[n_h][800], a1[][] = new double[n_h][800] ;
     static double z2[][] = new double[n_y][800], a2[][] = new double[n_y][800] ;
     static double cost ;
     static double trainData_T[][] = new double[4][800], y[][] = new double[3][800] ;
     static double dw2[][] = new double[3][800],db2[][] = new double[3][1],dw1[][] = new double[800][4],db1[][] = new double[800][1] ;
+    static double d_a1[][] = new double[n_h][800] ;
     
     Classify()
     {
@@ -256,6 +257,17 @@ public class Classify {
             sum = 0 ;
         }
     }
+    
+    public static void derivative_tanh()
+    {
+        for (int c = 0; c < n_h; c++)
+        {
+            for (int d = 0; d < 800; d++)
+            {
+                d_a1[c][d] = 1 - (a1[c][d] * a1[c][d]) ;
+             }
+        }
+    }
 
     public static void forwardPropagation()
     {   
@@ -289,7 +301,7 @@ public class Classify {
         {
             for (int d = 0; d < 800; d++)
             {
-                for (int k = 0; k < 3; k++)
+                for (int k = 0; k < 800; k++)
                 {
                     sum = sum + w2[c][k] * a1[k][d];
                 }
@@ -366,7 +378,7 @@ public class Classify {
         {
             for (int d = 0; d < 800; d++)
             {
-                for (int k = 0; k < 3; k++)
+                for (int k = 0; k < 800; k++)
                 {
                     sum = sum + dz2[c][k] * a1_T[k][d];
                 }
@@ -375,20 +387,125 @@ public class Classify {
                 sum = 0;
             }
         }
+        //db2 = (1/m)*np.sum(dz2, axis = 1)
+        for (int c = 0; c < 3; c++)
+        {           
+            for (int d = 0; d < 800; d++)
+            {
+                sum = sum + dz2[c][d] ;
+            }
+            db2[c][0] = sum ;
+            sum = 0 ;
+        }
         
+        //dz1 = (1/m)*np.dot(w2.T, dz2)*derivative_tanh(a1)
+        derivative_tanh() ;
+        double w2_T[][] = new double[n_h][n_y] ;
         
+        //transpose of w2 
+        for (int i = 0; i < n_y; ++i)
+        {
+            for (int j = 0; j < n_h; ++j)
+            {
+                w2_T[j][i] = w2[i][j];
+            }
+        }
+        
+        for (int c = 0; c < 800; c++)
+        {
+            for (int d = 0; d < 800; d++)
+            {
+                for (int k = 0; k < 3; k++)
+                {
+                    sum = sum + w2_T[c][k] * dz2[k][d];
+                }
+                dz1[c][d] = sum;
+                dz1[c][d] = (1/800.0)*dz1[c][d]*a1_T[c][d] ;
+                sum = 0;
+            }
+        }
+        //dw1 = (1/m)*np.dot(dz1, x.T)
+        for (int c = 0; c < 800; c++)
+        {
+            for (int d = 0; d < 4; d++)
+            {
+                for (int k = 0; k < 800; k++)
+                {
+                    sum = sum + dz1[c][k] * trainData[k][d];
+                }
+                dw1[c][d] = sum;
+                dw1[c][d] = (1/800.0)*dw1[c][d] ;
+                sum = 0;
+            }
+        }
+        //db1 = (1/m)*np.sum(dz1, axis = 1)
+        for (int c = 0; c < 800; c++)
+        {           
+            for (int d = 0; d < 800; d++)
+            {
+                sum = sum + dz1[c][d] ;
+            }
+            db1[c][0] = sum ;
+            sum = 0 ;
+        }       
     }
     
-   
-    public static void model(int itr , double learning_rate)
+    public static void updateParameters()
     {
-        splitData() ;
-        initialize_parameters() ;
+        //    w1 = w1 - learning_rate*dw1
+        for (int c = 0; c < 800; c++)
+        {           
+            for (int d = 0; d < 4; d++)
+            {
+                w1[c][d] = w1[c][d] - (learningRate*dw1[c][d]) ;
+            }
+        }
+        //    b1 = b1 - learning_rate*db1
+        for (int c = 0; c < 800; c++)
+        {           
+            for (int d = 0; d < 1; d++)
+            {
+                b1[c][d] = b1[c][d] - (learningRate*db1[c][d]) ;
+            }
+        }
+        //    w2 = w2 - learning_rate*dw2
+        for (int c = 0; c < 3; c++)
+        {           
+            for (int d = 0; d < 800; d++)
+            {
+                w2[c][d] = w2[c][d] - (learningRate*dw2[c][d]) ;
+            }
+        }
+        //    b2 = b2 - learning_rate*db2
+        for (int c = 0; c < 3; c++)
+        {           
+            for (int d = 0; d < 1; d++)
+            {
+                b2[c][d] = b2[c][d] - (learningRate*db2[c][d]) ;
+            }
+        }
+    }
+       
+    public static void model()
+    {
         int temp = 0 ;
-        while(temp<itr)
+        splitData() ;
+        getTranspose() ;
+        initialize_parameters() ;
+        
+        while(temp<iterations)
         {
             forwardPropagation() ;
+            costFunction() ;
             backwardPropagation() ;
+            updateParameters() ;
+            
+            if(temp%(iterations/10) == 0)
+            {
+                System.out.println("Cost after " +temp+ " iterations : " + cost) ;
+            }
+            
+            temp+=1 ;
         }       
     }
     
@@ -397,24 +514,24 @@ public class Classify {
         Classify obj = new Classify() ;
 //        printData() ;
         
-        splitData() ;  
-        getTranspose() ;
-        initialize_parameters() ;
-        forwardPropagation() ;
-        costFunction() ;
-        System.out.print(cost) ;
+//        splitData() ;  
+//        getTranspose() ;
+//        initialize_parameters() ;
+//        forwardPropagation() ;
+//        costFunction() ;
+//        backwardPropagation() ;
+//        updateParameters() ;
+//        System.out.print(cost) ;
 
-//        model(iterations , learningRate);  
-
+        model() ;
+//
 //        for(int i = 0 ; i < 3 ; i++)
 //        {
-//            for(int j = 0 ; j < 800 ; j++)
+//            for(int j = 0 ; j < 1 ; j++)
 //            {
-//                System.out.print(a2[i][j] + " ") ;
+//                System.out.print(b2[i][j] + " ") ;
 //            } 
 //            System.out.println() ;
-//        }
-
-           
+//        }           
     }    
 }
